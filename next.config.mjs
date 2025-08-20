f3a588648e074b00a6bbaf8606b9fcb10e9f2653
Meiki <meiki@example.com>
2025-08-20 18:58:25 +0900

    fix: use next.config.mjs for Next 14

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   experimental: {
     typedRoutes: true,
@@ -15,4 +14,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
